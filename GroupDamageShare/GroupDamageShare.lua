2db403c5d09727739d1f36eb32d41c881042eb79
Solinur <github@my-eg0.de>
2020-05-25 01:11:20 +0200

    API bump, release

--- GroupDamageShare/GroupDamageShare.lua
+++ GroupDamageShare/GroupDamageShare.lua
@@ -19,7 +19,7 @@ local activelyHidden = false
 -- Addon Namespace
 GDS = GDS or {}
 GDS.name = "GroupDamageShare"
-local addonVersion 	= "0.3.3"
+local addonVersion 	= "0.3.4"
 
 local function Print(message, ...)
 
